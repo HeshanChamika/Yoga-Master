0053839d3f545de3362562a3d30e6cc650d32129
HeshanChamika <it22194930@my.sliit.lk>
2024-07-01 14:26:00 +0530

    first commit

---
+++ frontend/src/pages/Home/PopularClasses/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = ({item}) => {
+
+   const {_id, name, image, availableSeats, price, totalEnrolled, } = item;
+
+  return (
+    <div>Card</div>
+  )
+}
+
+export default Card
