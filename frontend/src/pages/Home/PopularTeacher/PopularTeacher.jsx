015fdf19cf5a5bb694b9cd24e1a80fe5400df624
HeshanChamika <it22194930@my.sliit.lk>
2024-07-04 23:51:45 +0530

    login, register done

--- frontend/src/pages/Home/PopularTeacher/PopularTeacher.jsx
+++ frontend/src/pages/Home/PopularTeacher/PopularTeacher.jsx
@@ -12,7 +12,7 @@ const PopularTeacher = () => {
       }).catch((err) => {console.log(err)})
    }, []);
 
-   console.log(instructors)
+   // console.log(instructors)
   return (
    <div className='md:w-[80%] mx-auto my-36'>
    <div>
@@ -27,7 +27,7 @@ const PopularTeacher = () => {
       <div className='grid mb-28 md:grid-cols-2 lg:grid-cols-4 w-[90%] gap-4 mx-auto'>
          {
             instructors?.slice(0,4).map((instructor, i) => (
-               <div  className='flex dark:text-white hover:-translate-y-2 duration-200 cursor-pointer
+               <div key={i} className='flex dark:text-white hover:-translate-y-2 duration-200 cursor-pointer
                flex-col shadow-md py-8 px-10 md:px-8 rounded-md'>
                   <div className='flex-col flex gap-6 md:gap-8'>
                      <img className='rounded-full border-4 border-gray-300 h-24 w-24 mx-auto' src={instructor?.instructor?.photoUrl || `${img}`} alt="" />
